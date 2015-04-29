b0f492e2ce0c2a559df66889f699dfca507e5ab7
Nate M <Mr.awsmness@gmail.com>
2015-04-29 10:54:29 -0700

    test

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Quiz 2</title>
+	<script type="text/javascript" src="main.js"></script>
+</head>
+<body>
+	<div class="dan">
+		<button class="submit1" type="submit1">Click Me</button>
+	</div>
+	<div id="iwao">
+		<button class="submit2" type="submit2">Click Me</button>
+	</div>
+</body>
+<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+</html>
---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('.dan').click(function(){
+		console.log("Hey you clicked me!");
+	});
+	$('#iwao').click(function(){
+		$('#iwao').fadOut('slow');
+	});
+});
